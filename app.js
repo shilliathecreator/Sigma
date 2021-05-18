47dd25c0ac65a95a4d8c12a651a66ef4a76e6b6c
shilliathecreator <84385679+shilliathecreator@users.noreply.github.com>
2021-05-18 14:24:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,25 @@ var firebaseConfig = {
   messagingSenderId: "sender-id",
   appId: "app-id",
   measurementId: "G-measurement-id",
+  <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAbn9TUBPQHJsH5R-P2WOPqQD1h3yeNV40",
+    authDomain: "deneme-194ae.firebaseapp.com",
+    projectId: "deneme-194ae",
+    storageBucket: "deneme-194ae.appspot.com",
+    messagingSenderId: "75403386082",
+    appId: "1:75403386082:web:38d0ed650fe7830a0892bb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
 };
 
      
