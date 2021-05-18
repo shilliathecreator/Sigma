a4190b8b25bdfe48ad04a921c4aa107fd1b03449
shilliathecreator <84385679+shilliathecreator@users.noreply.github.com>
2021-05-18 15:10:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ var firebaseConfig = {
   messagingSenderId: "sender-id",
   appId: "app-id",
   measurementId: "G-measurement-id",
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAbn9TUBPQHJsH5R-P2WOPqQD1h3yeNV40",
+  authDomain: "deneme-194ae.firebaseapp.com",
+  databaseURL: "https://deneme-194ae-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "deneme-194ae",
+  storageBucket: "deneme-194ae.appspot.com",
+  messagingSenderId: "75403386082",
+  appId: "1:75403386082:web:38d0ed650fe7830a0892bb",
+  measurementId: "G-NQ7BHKT6VG"
+};
 };
 
      
